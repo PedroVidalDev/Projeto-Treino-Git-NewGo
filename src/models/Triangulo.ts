fff434824c9091557a222b8eb066f60fe2f9d48e
PedroVidalDev <pedrohvidals@gmail.com>
2024-07-30 18:31:41 -0300

    feat: criando classe triangulo com constructor e funcao base pra calcular a area

---
+++ src/models/Triangulo.ts
@@ -0,0 +1,18 @@
+class Triangulo{
+    private ladoA : Number;
+    private ladoB : Number;
+    private ladoC : Number;
+
+    constructor(ladoA : Number, ladoB : Number, ladoC : Number){
+        this.ladoA = ladoA;
+        this.ladoB = ladoB;
+        this.ladoC = ladoC;
+    }
+
+    calcularArea() {
+
+        return 0;
+    }
+}
+
+export default Triangulo;
