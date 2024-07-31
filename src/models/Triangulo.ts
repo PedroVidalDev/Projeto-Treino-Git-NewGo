f637e03c1c3780e483b3ef65426f94c581a0ce42
PedroVidalDev <pedrohvidals@gmail.com>
2024-07-31 14:34:31 -0300

    Revert "feat: criando funcoes para calcular radianos e para adquirir area usando apenas um lado e um angulo do triangulo retangulo"
    This reverts commit 556a4f1095c340b85885079c845e70cb3ae27ac2.

--- src/models/Triangulo.ts
+++ src/models/Triangulo.ts
@@ -29,12 +29,8 @@ class Triangulo{
         return this.ladoC;
     }
 
-    public grausParaRadianos(angulo : number) : number{
-        return angulo * (Math.PI / 180);
-    }
-
-    public calcularAltura() : number{
-        let altura: number = Math.sqrt(this.ladoB**2 - (this.getLadoA() / 2)**2)
+    calcularAltura() : number{
+        let altura: number = Math.sqrt((this.getLadoA() / 2)**2 + this.getLadoB()**2)
 
         return altura;
     }
@@ -60,19 +56,19 @@ class Triangulo{
     public calcularAreaTrianguloSemUmLadoEUmAngulo() : number {
 
         if(this.ladoA != null && this.ladoB != null && this.ladoC == null){
-            let seno : number = Math.sin(this.grausParaRadianos(this.anguloAB));
+            let seno : number = Math.sin(this.anguloAB * (Math.PI / 180));
 
             return (this.ladoA * this.ladoB * seno) / 2;
         }
 
         else if(this.ladoB != null && this.ladoC != null && this.ladoA == null){
-            let seno : number = Math.sin(this.grausParaRadianos(this.anguloBC));
+            let seno : number = Math.sin(this.anguloBC * (Math.PI / 180));
 
             return (this.ladoB * this.ladoC * seno) / 2;
         }
 
         else if(this.ladoC != null && this.ladoA != null && this.ladoB == null){
-            let seno : number = Math.sin(this.grausParaRadianos(this.anguloCA));
+            let seno : number = Math.sin(this.anguloCA * (Math.PI / 180));
 
             return (this.ladoC * this.ladoA * seno) / 2;
         }
@@ -82,52 +78,6 @@ class Triangulo{
         let semi : number = (this.ladoA + this.ladoB + this.ladoC) / 2;
 
         return Math.sqrt(semi * (semi - this.ladoA) * (semi - this.ladoB) * (semi - this.ladoC));
-    }
-
-    public calcularAreaTrianguloComUmLadoEUmAngulo() : number {
-            if(this.ladoA != null){
-                if(this.anguloAB != null){
-                    this.ladoC = Math.tan(this.grausParaRadianos(this.anguloAB)) * this.ladoA;
-
-                    return (this.ladoC * this.ladoA) / 2;
-                }
-
-                else if(this.anguloBC != null){
-                    this.ladoC = this.ladoA / Math.tan(this.grausParaRadianos(this.anguloBC));
-
-                    return (this.ladoC * this.ladoA) / 2;
-                }
-            }
-
-            else if(this.ladoC != null){
-                if(this.anguloAB != null){
-                    this.ladoA = this.ladoC / Math.tan(this.grausParaRadianos(this.anguloAB));
-
-                    return (this.ladoC * this.ladoA) / 2;
-                }
-
-                else if(this.anguloBC != null){
-                    this.ladoA = this.ladoC * Math.tan(this.grausParaRadianos(this.anguloBC));
-
-                    return (this.ladoA * this.ladoC) / 2;
-                }
-            }
-
-            else if(this.ladoB != null){
-                if(this.anguloAB != null){
-                    this.ladoA = Math.cos(this.grausParaRadianos(this.anguloAB)) * this.ladoB;
-                    this.ladoC = Math.sqrt(this.ladoB**2 - this.ladoA**2);
-
-                    return (this.ladoA * this.ladoC) / 2;
-                }
-
-                else if(this.anguloBC != null){
-                    this.ladoA = Math.sin(this.grausParaRadianos(this.anguloBC)) * this.ladoB;
-                    this.ladoC = Math.sqrt(this.ladoB**2 - this.ladoA**2);
-
-                    return (this.ladoA * this.ladoC) / 2;
-                }
-            }
     }
     
 }
