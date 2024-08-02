8862e6fca770cf745dfe6201a5c198f50cf31d21
PedroVidalDev <pedrohvidals@gmail.com>
2024-08-01 22:07:37 -0300

    refactor: removendo o atributo altura da classe de triangulo isosceles e removendo locais que esse atributo seja mencionado

--- src/models/TrianguloIsosceles.ts
+++ src/models/TrianguloIsosceles.ts
@@ -1,20 +1,13 @@
 import Triangulo from "./Triangulo.js";
 
 class TrianguloIsosceles extends Triangulo{
-    private altura : number;
-
-    constructor(base : number, lado : number, altura : number = null){
+    constructor(base : number, lado : number){
         super(base, lado, lado);
-        this.altura = altura;
-    }
-
-    public getAltura() : number {
-        return this.altura;
     }
 
     public calcularAltura(): number {
-        this.altura = Math.sqrt((this.ladoB**2) - ((this.ladoA / 2)**2))
-        return this.altura;
+        let altura : number = Math.sqrt((this.ladoB**2) - ((this.ladoA / 2)**2))
+        return altura;
     }
 
     public calcularArea() : number {
